bcfee9b6b1372eee14e5e2364de9b08c2cb031c1
egbendito <e.gbendito@gmail.com>
2024-07-17 10:58:30 +0000

    updates

--- 0_download.R
+++ 0_download.R
@@ -1,5 +1,5 @@
 # ROI
-iso <- c("NGA")
+iso <- c("RWA")
 
 origin<- getwd()
 
@@ -15,27 +15,26 @@ if(!file.exists("./data/input/gadm/roi.gpkg")){
 
   # Merge files
   # Check validity of geometries
-  if (length(list.files("./data/input/gadm/", pattern = ".gpkg")) > 1) {
-    fname <- paste0("./data/input/gadm/gadm41_", iso[1], ".gpkg")
-    A <- sf::st_read(paste0("./data/input/gadm/gadm41_", iso[1], ".gpkg"), layer = sf::st_layers(fname)[[1]][1], quiet = T)
-    for (l in sf::st_layers(fname)[2]) {
+  if(length(list.files("./data/input/gadm/", pattern = ".gpkg")) == 1){
+    f <- list.files("./data/input/gadm/", pattern = ".gpkg")
+    iso <- substr(gsub(".gpkg","",f), 8,10)
+    fname <- paste0("./data/input/gadm/", f)
+    for (l in sf::st_layers(fname)[1]) {
       pol <- sf::st_read(fname, layer = l[length(l)], quiet = T)
-      A <- sf::st_union(dplyr::bind_rows(list(A,pol)), by_feature = T)
+      A <- sf::st_union(pol, by_feature = T)
     }
-    for (f in list.files("./data/input/gadm/", pattern = ".gpkg")[2:length(list.files("./data/input/gadm/",pattern = ".gpkg"))]) {
+  }
+  else {
+    A <- sf::st_read(paste0("./data/input/gadm/gadm41_", iso[1], ".gpkg"), layer = "ADM_ADM_2", quiet = T)
+    for (f in list.files("./data/input/gadm/",
+                         pattern = ".gpkg")[2:length(list.files("./data/input/gadm/",
+                                                                pattern = ".gpkg"))]) {
       iso <- substr(gsub(".gpkg","",f), 8,10)
       fname <- paste0("./data/input/gadm/", f)
-      for (l in sf::st_layers(fname)[[1]][2:length(sf::st_layers(fname)[[1]])]) {
-        pol <- sf::st_read(fname, layer = l, quiet = T)
+      for (l in sf::st_layers(fname)[1]) {
+        pol <- sf::st_read(fname, layer = l[length(l)], quiet = T)
         A <- sf::st_union(dplyr::bind_rows(list(A,pol)), by_feature = T)
       }
-    }
-  } else {
-    fname <- paste0("./data/input/gadm/gadm41_", iso[1], ".gpkg")
-    A <- sf::st_read(paste0("./data/input/gadm/gadm41_", iso[1], ".gpkg"), layer = sf::st_layers(fname)[[1]][1], quiet = T)
-    for (l in sf::st_layers(fname)[[1]][2:length(sf::st_layers(fname)[[1]])]) {
-      pol <- sf::st_read(fname, layer = l, quiet = T)
-      A <- sf::st_union(dplyr::bind_rows(list(A,pol)), by_feature = T)
     }
   }
   sf::write_sf(obj = A, dsn = "./data/input/gadm/roi.gpkg", layer = "roi", append = FALSE)
@@ -62,7 +61,7 @@ for (i in seq_along(vars)) {
   for (year in years){
     if(!file.exists(paste0("./data/input/", vname, "/", year, ".nc"))){
       if(vname == "chirps"){
-        file <- list.files("/home/jovyan/common_data/chirps_af/chirps/netcdf", full.names = TRUE, pattern = paste0(year, collapse = '|'))
+        file <- list.files("/home/jovyan/common_data/chirps_af/netcdf", full.names = TRUE, pattern = paste0(year, collapse = '|'))
       }
       else {
         file <- list.files("/home/jovyan/common_data/chirts/netcdf/tmax/africa", full.names = TRUE, pattern = paste0(year, collapse = '|'))
